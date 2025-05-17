06d23dda5b45566d2657a0474275b76419ebdf8d
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-05-16 22:36:39 -0300

    Function CallBack

---
+++ JS-Functions/funcoes/aula-19/callback.js
@@ -0,0 +1,25 @@
+
+function exec(fn, a, b){
+    return fn(a,b)
+}
+
+const somarNoTerminal = (x, y) => console.log(x + y)
+const subtrairNoTerminal = (x, y) => console.log(x - y)
+const subtrair = (w, z) => w - z
+
+exec(somarNoTerminal, 56, 38)
+exec(subtrairNoTerminal, 182, 27)
+
+const r = exec(subtrair, 50, 25)
+console.log(r)
+
+
+//const cb = () => console.log('Exec...')
+//setInterval(cb, 1000)
+
+//setInterval(() => console.log('Exec 2...'), 1000)
+
+
+setInterval (function(){
+    console.log('Exec 3...')
+}, 1000)

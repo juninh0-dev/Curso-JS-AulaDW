0325de26045a6b6ac6e97ee37d116481df8fc980
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-05-28 14:59:25 -0300

    Challenge, map, reduce and filter

---
+++ JS-Functions/funcoes/aula-27/desafio.js
@@ -0,0 +1,13 @@
+const carrinho = [
+    {name: 'Caneta', amount: 10, price: 7.99, fragil: true},
+    {name: 'Impressora', amount: 1, price: 649.50, fragil: true},
+    {name: 'Caderno', amount: 4, price: 27.10, fragil: false},
+    {name: 'Lapis', amount: 3, price: 5.82, fragil: false},
+    {name: 'Tesoura', amount: 1, price: 19.20, fragil: true},
+]
+
+// filter, map, reduce
+
+// 1. fragil: true
+// 2. amount * price -> total
+// 3. media totais

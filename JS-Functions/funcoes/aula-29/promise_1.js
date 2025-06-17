69625cd6d4ee051739887cb8124a594718c4d571
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-06-17 17:26:52 -0300

    Finishing the firts class of Promise function

--- JS-Functions/funcoes/aula-29/promise_1.js
+++ JS-Functions/funcoes/aula-29/promise_1.js
@@ -5,18 +5,30 @@ console.log(a)
 //    cumprirPromessa(3, 4)//Ele sõ cumpre uma promessa mesmo declarando 2 parâmetros
 //})
 
-let p = new Promise(function (cumprirPromessa) {
-    cumprirPromessa(
+const primeiroElemento = array => array[0]
+
+const primeiraLetra = string => string[0]
+
+
+const letraMinuscula = letra => letra.toLowerCase()
+
+let p = new Promise(function (resolve) {
+    resolve(
         //x: 3,
         //y: 4
         ['Ana', 'Bia', 'Carlos', 'Mario'])
-})
+}).then(primeiroElemento)
+    .then(primeiraLetra)
+    .then(letraMinuscula)
+    .then(console.log)
 
-p
-    .then(valor => valor[0])
-    .then(primeiro => primeiro[0])
-    .then(letra => letra.toLowerCase())
-    .then(letraMinuscula => console.log(letraMinuscula))
+
+
+//p
+//    .then(valor => valor[0])
+//    .then(primeiro => primeiro[0])
+//    .then(letra => letra.toLowerCase())
+//    .then(letraMinuscula => console.log(letraMinuscula))
 
 
 //p.then(valor => console.log(valor))

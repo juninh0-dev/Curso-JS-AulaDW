89b7c31d68fd9a21333b3f7669248bcc9cb61436
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-06-22 21:22:47 -0300

    Adding the last Async/Await class of course JS

--- JS-Functions/funcoes/aula-36/async_await_2.js
+++ JS-Functions/funcoes/aula-36/async_await_2.js
@@ -1,8 +1,37 @@
-function gerarNumerosEntre(min, max){
-    if(min > max) [max, min] = [min, max]
-    return new Promise(resolve => {
+function gerarNumerosEntre(min, max, numerosProibidos) {
+    if (min > max) [max, min] = [min, max]
+    return new Promise((resolve, reject) => {
         const fator = max - min + 1
-            const aleatorio = parseInt(Math.random() * fator) + min
+        const aleatorio = parseInt(Math.random() * fator) + min
+        if (numerosProibidos.includes(aleatorio)) {
+            reject('Número repetido!')
+
+        }
         resolve(aleatorio)
     })
 }
+
+async function gerarMegaSena(qntdeNumeros, tentativas = 1) {
+    try {
+        const numeros = []
+        for (let _ of Array(qntdeNumeros).fill()) {
+            numeros.push(await gerarNumerosEntre(1, 60, numeros))
+        }
+        return numeros
+    } catch (e) {
+        if (tentativas > 10) {
+            throw "Que CHATO!"
+        } else {
+            return gerarMegaSena(qntdeNumeros, tentativas + 1)
+        }
+    }
+
+}
+
+gerarMegaSena(20)
+    .then(console.log)
+    .catch(console.log)
+
+gerarNumerosEntre(1, 5, [1, 2, 4])
+    .then(console.log)
+    .catch(console.log)
